e8f39268e0644a3d37b051eb8bb861061986a985
Lamine <kechac.l@gmail.com>
2016-04-08 13:32:14 +0200

    Added URL without underline
    Signed-off-by: Lamine

--- app/src/main/java/com/instaclonegram/fragments/FragmentProfile.java
+++ app/src/main/java/com/instaclonegram/fragments/FragmentProfile.java
@@ -4,6 +4,9 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.URLSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.instaclonegram.R;
+import com.instaclonegram.library.URLSpanNoUnderline;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -29,6 +33,24 @@ public class FragmentProfile extends Fragment {
         CircleImageView profile = (CircleImageView)rootView.findViewById(R.id.imgview_profile);
         profile.setImageResource(R.drawable.kevinsys);
 
+        initializeViews(rootView);
+
+        return rootView;
+    }
+
+    public static void removeUnderlines(Spannable p_Text) {
+        URLSpan[] spans = p_Text.getSpans(0, p_Text.length(), URLSpan.class);
+
+        for(URLSpan span:spans) {
+            int start = p_Text.getSpanStart(span);
+            int end = p_Text.getSpanEnd(span);
+            p_Text.removeSpan(span);
+            span = new URLSpanNoUnderline(span.getURL());
+            p_Text.setSpan(span, start, end, 0);
+        }
+    }
+
+    private void initializeViews(View rootView) {
         TextView tv_profile_username = (TextView)rootView.findViewById(R.id.tv_profile_username);
         TextView tv_profile_description = (TextView)rootView.findViewById(R.id.tv_profile_description);
         TextView tv_profile_link = (TextView)rootView.findViewById(R.id.tv_profile_link);
@@ -41,16 +63,17 @@ public class FragmentProfile extends Fragment {
 
         Button editprofile = (Button)rootView.findViewById(R.id.button_editprofile);
         editprofile.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.darkgreycolor));
-        tv_profile_username.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.blackcolor));
-        tv_profile_description.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.blackcolor));
-        tv_profile_link.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.blackcolor));
+        tv_profile_username.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.darkgreycolor1));
+        tv_profile_description.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.darkgreycolor1));
+        tv_profile_link.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.linkbluecolor));
         tv_following.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.greycolor1));
         tv_followers.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.greycolor1));
         tv_posts.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.greycolor1));
-        tv_following_cnt.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.blackcolor));
+        tv_following_cnt.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.darkgreycolor1));
         tv_followers_cnt.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.darkgreycolor1));
         tv_posts_cnt.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.darkgreycolor1));
 
-        return rootView;
+        removeUnderlines((Spannable) tv_profile_link.getText());
     }
+
 }
